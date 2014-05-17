ca7d9eea93300c37ec9984d27582e0863e78977e
Olav Vahtras <vahtras@kth.se>
2014-05-18 00:36:18 +0200

    remove junk

--- test/ang.py
+++
@@ -1,7 +0,0 @@
-import sys
-from math import *
-theta = float(sys.argv[1])
-thetar = theta*pi/180
-angr  = acos(-0.5*sin(thetar)**2 + cos(thetar)**2)
-ang = angr*180/pi
-print ang
--- test/iang.py
+++
@@ -1,7 +0,0 @@
-import sys
-from math import *
-ang = float(sys.argv[1])
-angr = ang*pi/180
-thetar  = pi-asin(sqrt(2*(1-cos(angr))/3))
-theta = thetar*180/pi
-print theta

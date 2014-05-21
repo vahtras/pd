82834a140aff793f4891a0fa0b3ea56250feb235
Olav Vahtras <vahtras@kth.se>
2014-05-21 13:06:51 +0200

    Add test of orthogonal polarizabiliey

--- test/test_particles.py
+++ test/test_particles.py
@@ -496,16 +496,20 @@ class PointDipoleListTest(unittest.TestCase):
             ]])
         np.testing.assert_almost_equal(alphas, alphas_ref)
 
-    def test_induced_dipole_on_one_atom(self):
+    def test_induced_parallel_dipole_on_one_atom(self):
         h2 = PointDipoleList(iterize(H2["POTFILE"]))
         E_external = np.array([0., 0., 1.,])
         p_ind_ref =  np.array([0., 0., 0.95899377])
         h2.solve_scf_for_external(E_external, max_it = 100)
         np.testing.assert_almost_equal(h2[0].p, p_ind_ref, decimal=6)
-        
 
+    def test_induced_orthogonal_dipole_on_one_atom(self):
+        h2 = PointDipoleList(iterize(H2["POTFILE"]))
+        E_external = np.array([1., 0., 0.,])
+        p_ind_ref =  np.array([0.11894578, 0., 0.])
+        h2.solve_scf_for_external(E_external, max_it = 100)
+        np.testing.assert_almost_equal(h2[0].p, p_ind_ref, decimal=6)
 
-                        
         
 if __name__ == "__main__":
     unittest.main()

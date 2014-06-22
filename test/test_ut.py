4361a9478162181336d186653a070a802afb7c8b
Olav Vahtras <vahtras@kth.se>
2014-06-22 11:00:14 -0500

    Upper triangular looping utility

--- test/test_ut.py
+++ test/test_ut.py
@@ -9,4 +9,15 @@ class TestTriangular(TestCase):
         two_seq = ((0, 0), (0, 1), (0, 2), (1, 1), (1, 2), (2, 2))
         self.assertEqual(tuple(upper_triangular(2)), two_seq)
 
+    def test_threedim(self):
+        three_seq = (
+            (0, 0, 0), (0, 0, 1), (0, 0, 2),
+            (0, 1, 1), (0, 1, 2),
+            (0, 2, 2),
+                       (1, 1, 1), (1, 1, 2),
+                       (1, 2, 2),
+                                  (2, 2, 2)
+            )
+        self.assertEqual(tuple(upper_triangular(3)), three_seq)
+
 
--- ut.py
+++ ut.py
@@ -1,4 +1,10 @@
-def upper_triangular(n):
-    for i in range(3):
-        for j in range(i, 3):
-            yield i, j
+def upper_triangular(n, start=0):
+    """Recursive generator for triangular looping of Carteesian tensor"""
+    if n > 2:
+        for i in range(start, 3):
+            for j in upper_triangular(n-1, start=i):
+                yield (i,) + j
+    else:
+        for i in range(start, 3):
+            for j in range(i, 3):
+                yield i, j

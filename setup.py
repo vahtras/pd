febcba2749307532a23134e847689666bafda3af
Ignat Harczuk <ignathe@gmail.com>
2016-02-03 09:55:15 +0100

    new tree and working setup file

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
-from distutils.core import setup
-from distutils.extension import Extension
+#from distutils.core import setup
+#from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
-
-from setuptools import setup, Command
+from setuptools import setup, Command, Extension
 
 import numpy
 
 ext_module = Extension(
-        "optimized_func",
-        ["optimized_func.pyx"],
+        "pd/optimized_func",
+        ["pd/optimized_func.pyx"],
         extra_compile_args = ["-O3", "-ffast-math", "-march=native",'-fopenmp', ],
         extra_link_args = ['-fopenmp'], 
         include_dirs = [ numpy.get_include()],
         )
 
 setup(
-    version = "0.1",
     name = "pd",
-    packages = ['.'],
+    version = "0.1",
+    packages = ['pd'],
     ext_modules = [ext_module],
     cmdclass = { 'build_ext' : build_ext },
     setup_requires = [ 'numpy==1.9.2', 'cython==0.20.1' ],
